58db74166c28181a3bbdb1eed8d06a518297c35a
Harvey Gleeson <harveygleeson@gmail.com>
2023-02-20 15:05:41 +0000

    Add styling and state for todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todos";
 
 const App = () => {
-  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodoList((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
   return (
     <div>
-      <Todos items={todos} />
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todoList} />
     </div>
   );
 };
